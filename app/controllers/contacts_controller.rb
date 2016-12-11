f1ef4b7cb9638855879fe5b51b27de00aff8cb76
jkwellyn <jbchiu.1018@gmail.com>
2016-12-11 22:27:52 +0000

    add custom contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,8 @@
 class ContactsController < ApplicationController
-    
   def new
     @contact = Contact.new
   end
-  
+
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
@@ -15,10 +14,9 @@ class ContactsController < ApplicationController
     end
   end
 
-private
+  private
 
   def contact_params
     params.require(:contact).permit(:name, :email, :comments)
   end
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,11 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 1) do
-
-  create_table "contacts", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.text     "comments"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'contacts', force: :cascade do |t|
+    t.string   'name'
+    t.string   'email'
+    t.text     'comments'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
